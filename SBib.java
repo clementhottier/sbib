50f28353ac764e027d87dc258326a0f5e9d8d673
chottier <clement.hottier@obspm.fr>
2016-10-26 17:52:25 +0200

    new way to handle opening file

--- SBib.java
+++ SBib.java
@@ -7,22 +7,25 @@ import java.util.ArrayList;
 public class SBib {
   public static void main (String[] args){
     String bibPath = "biblio_these.bib";
-    File bibFile = null;
     System.out.println("\tSBib launch :");
 
-    bibFile = new File(bibPath);
-    readFile(bibFile);
+    readFile(bibPath);
 
   }
 
-  public static ArrayList readFile(File bibFile){
+  public static ArrayList readFile(String bibPath){
   // method to read bibFile and put all the Entry in a list containing BibEntry instanc
   ArrayList bibList = new ArrayList();
     try {
-      BufferedReader bibReader = new BufferedReader(new FileReader(bibFile));
+      BufferedReader bibReader = new BufferedReader(new FileReader(new File(bibPath)));
     }
     catch (FileNotFoundException e){
       System.out.println("file "+bibFile.toString()+" not found");
+    }
+    finally{
+      if (bibReader!=null){
+        bibReader.close();
+      }
     }
   return bibList;
   }
